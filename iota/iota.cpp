ce198c3722e48113af7d5ac3ddb13a3b3c0679bc
Paul R <paul@epicyclism.com>
2018-10-16 16:46:40 +0100

    Add files via upload

---
+++ iota/iota.cpp
@@ -0,0 +1,36 @@
+﻿// iota.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+#include <array>
+#include "iota.h"
+
+using alphabet_t = std::array<char, 26> ;
+constexpr alphabet_t MakeAlphabet()
+{
+	alphabet_t rv{};
+	epicyclism::iotum_base<char> gen('A');
+	for (auto& c : rv)
+	{
+		c = *gen;
+		++gen;
+	}
+
+	return rv;
+}
+
+constexpr alphabet_t AL = MakeAlphabet();
+
+int main()
+{
+	using epicyclism::iota;
+	for (int i : iota{ 0, 10 })
+		std::cout << i << " ";
+	std::cout << "\n";
+
+	for (auto c : AL)
+		std::cout << c;
+	std::cout << "\n";
+
+	return 0;
+}
---
+++ iota/iota.h
@@ -0,0 +1,80 @@
+﻿#pragma once
+
+// iota.h
+//
+// iterators that act like values
+//
+
+namespace epicyclism
+{
+	template<typename V> class iotum_base
+	{
+	public:
+		using value_type = V;
+	private:
+		V val_;
+	public:
+		constexpr iotum_base ( V v) : val_(v)
+		{}
+		constexpr V& operator++()
+		{
+			++val_;
+			return val_;
+		}
+		constexpr V operator++(int)
+		{
+			V v(val_);
+			++val_;
+			return v;
+		}
+		constexpr V& operator--()
+		{
+			--val_;
+			return val_;
+		}
+		constexpr V operator--(int)
+		{
+			V v(val_);
+			--val_;
+			return v;
+		}
+		constexpr V operator*() const
+		{
+			return val_;
+		}
+		constexpr operator V() const
+		{
+			return val_;
+		}
+	};
+	template<typename V> constexpr bool operator != (iotum_base<V> const& l, iotum_base<V> const& r)
+	{
+		return static_cast<V>(l) != static_cast<V>(r);
+	}
+	template<typename V> constexpr bool operator < (iotum_base<V> const& l, iotum_base<V> const& r)
+	{
+		return static_cast<V>(l) < static_cast<V>(r);
+	}
+	template<typename V> class iota_base
+	{
+	public:
+		using value_type = V;
+	private:
+		iotum_base<V> begin_;
+		iotum_base<V> end_;
+	public:
+		iota_base ( V begin, V end) : begin_(begin), end_(end)
+		{}
+		constexpr iotum_base<V> begin() const
+		{
+			return begin_;
+		}
+		constexpr iotum_base<V> end() const
+		{
+			return end_;
+		}
+	};
+
+	using iotum = iotum_base<int>;
+	using iota = iota_base<int>;
+}
